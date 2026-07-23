cb49e2def7ff8f77549acb647fa8666b751d3c7f
Bryce Berdijo <bryce.berdijo@student.ateneo.edu>
2026-07-23 18:14:18 +0800

    I have set up the discord.js files and the bot should be fine

---
+++ index.js
@@ -0,0 +1,7 @@
+const {token} = require("./config.json");
+const {Client, GatewayIntentBits} = require("discord.js");
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
+
+client.login(token);
+
