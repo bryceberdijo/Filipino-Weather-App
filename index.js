bba35ce9f50d24481f17857ef824b7225d7459af
Bryce Berdijo <bryce.berdijo@student.ateneo.edu>
2026-07-23 19:39:58 +0800

    removed config.json in order to have better privacy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {token} = require("./config.json");
+const {token} = require("../Security/Weather App/config.json");
 const {Client, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
