3e458dc5ebdb2f9db2ca177f9dcb933ab9294a52
Bryce Berdijo <bryce.berdijo@student.ateneo.edu>
2026-07-24 20:46:51 +0800

    I have added a comment and modified the config.json file to have better security and explain the reasoning hiding the token

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {token} = require("../Security/Weather App/config.json");
+const {token} = require("../Security/Weather App/config.json"); //This is for the purposes of keeping the token for the discord bot
 const {Client, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
